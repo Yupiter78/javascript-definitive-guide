3e85300fc0d7542f562731be67eaff1c207e32b5
js-neo <yuriy.jsx@gmail.com>
2024-08-14 00:48:03 +0300

    feat: add class histogram

--- histograms.js
+++ histograms.js
@@ -8,3 +8,20 @@ class DefaultMap extends Map {
         return this.has(key) ? super.get(key) : this.defaultValue;
     }
 }
+
+class Histogram {
+    constructor() {
+        this.letterCounts = new DefaultMap(0);
+        this.totalLetters = 0;
+    }
+
+    add(text) {
+        text = text.replace(/\s/g, "").toUpperCase();
+
+        for (let character of text) {
+            let count = this.letterCounts.get(character);
+            this.letterCounts.set(character, count + 1);
+            this.totalLetters++;
+        }
+    }
+}
