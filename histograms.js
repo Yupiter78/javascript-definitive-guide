a2a054101f64e0d848bda57169e50547a2efa64c
js-neo <yuriy.jsx@gmail.com>
2024-08-14 21:34:20 +0300

    feat: add toString method to Histogram class

--- histograms.js
+++ histograms.js
@@ -18,11 +18,32 @@ class Histogram {
     add(text) {
         text = text.replace(/\s/g, "").toUpperCase();
 
-        this.letterCounts = Array.from(text).reduce(( acc, cur) => {
+        this.letterCounts = Array.from(text).reduce((acc, cur) => {
             let count = acc.get(cur);
             acc.set(cur, count + 1);
             this.totalLetters++;
             return acc;
         }, this.letterCounts);
+    }
+
+    toString() {
+        let entries = [...this.letterCounts];
+        entries.sort(([aChar, aCount], [bChar, bCount]) => {
+            if (aCount === bCount) {
+                return aChar < bChar ? -1 : 1;
+            } else {
+                return bCount - aCount;
+            }
+        });
+
+        for (let entry of entries) {
+            entry[1] = (entry[1] / this.totalLetters) * 100;
+        }
+
+        entries = entries.filter((entry) => entry[1] >= 1);
+        let lines = entries.map(
+            ([l, n]) => `${l}: ${"#".repeat(Math.round(n))}${n.toFixed(2)}%`
+        );
+        return lines.join("\n");
     }
 }
